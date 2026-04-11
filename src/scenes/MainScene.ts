d87d4677f0b46536c4c0348f967cc98eafde6647
sanghyeon <jkn17082@naver.com>
2026-04-11 15:47:22 +0900

    add flying state

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -2,7 +2,14 @@ import Phaser from 'phaser'
 
 export class MainScene extends Phaser.Scene {
   private bird?: Phaser.GameObjects.Image
+  private groundTiles: Phaser.GameObjects.Image[] = []
+  private pipePair: Phaser.GameObjects.Image[] = []
+  private startPanel?: Phaser.GameObjects.Image
+  private guideText?: Phaser.GameObjects.Text
   private isWingUp = true
+  private isPlaying = false
+  private groundSpeed = 220
+  private pipeSpeed = 160
 
   constructor() {
     super('MainScene')
@@ -22,15 +29,21 @@ export class MainScene extends Phaser.Scene {
 
     this.add.image(width / 2, height / 2, 'background').setDisplaySize(width, height)
 
-    this.add.image(width + 40, -150, 'pipe').setFlipY(true)
-    this.add.image(width + 40, height - 260, 'pipe')
-    this.add.image(width / 2, height - 60, 'ground').setDisplaySize(width, 120)
+    this.pipePair = [
+      this.add.image(width + 120, -150, 'pipe').setFlipY(true),
+      this.add.image(width + 120, height - 260, 'pipe'),
+    ]
+
+    this.groundTiles = [
+      this.add.image(0, height - 120, 'ground').setOrigin(0, 0).setDisplaySize(width, 120),
+      this.add.image(width, height - 120, 'ground').setOrigin(0, 0).setDisplaySize(width, 120),
+    ]
 
     this.bird = this.add.image(220, height / 2, 'bird-up').setScale(1.35)
 
-    this.add.image(width / 2, 130, 'start-panel')
+    this.startPanel = this.add.image(width / 2, 130, 'start-panel')
 
-    this.add
+    this.guideText = this.add
       .text(width / 2, 250, 'Asset preview scene', {
         fontFamily: 'Verdana, sans-serif',
         fontSize: '22px',
@@ -49,5 +62,42 @@ export class MainScene extends Phaser.Scene {
       },
     })
 
+    this.input.on('pointerdown', () => this.startGame())
+    this.input.keyboard?.on('keydown-SPACE', () => this.startGame())
+  }
+
+  update(_: number, delta: number) {
+    if (!this.isPlaying) {
+      return
+    }
+
+    const groundMoveAmount = (this.groundSpeed * delta) / 1000
+    const pipeMoveAmount = (this.pipeSpeed * delta) / 1000
+    const { width } = this.scale
+
+    this.groundTiles.forEach((ground) => {
+      ground.x -= groundMoveAmount
+
+      if (ground.x <= -width) {
+        ground.x += width * this.groundTiles.length
+      }
+    })
+
+    this.pipePair.forEach((pipe) => {
+      pipe.x -= pipeMoveAmount
+
+      if (pipe.x < -80) {
+        pipe.x = width + 120
+      }
+    })
+  }
+
+  private startGame() {
+    if (this.isPlaying) {
+      return
+    }
+
+    this.isPlaying = true
+    this.startPanel?.setVisible(false)
   }
 }
